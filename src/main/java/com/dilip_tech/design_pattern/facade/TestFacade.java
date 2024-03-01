dde1bf9361e33d1ac91cb8127258ecadf5604a18
rudra <dilip.tech.developer@gmail.com>
2024-03-01 15:58:23 +0530

    add test

--- src/main/java/com/dilip_tech/design_pattern/facade/TestFacade.java
+++ src/main/java/com/dilip_tech/design_pattern/facade/TestFacade.java
@@ -1,6 +1,7 @@
 package com.dilip_tech.design_pattern.facade;
 
 public class TestFacade {
+    //test
     public static void main(String[] args) {
         OrderFacade facade=new OrderFacade();
         facade.processOrder("Iphone",100);
